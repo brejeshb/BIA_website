871973088704f33f56313e5e84ed756821d61761
Brejeshb <brejeshb2023@Jebresh>
2024-08-03 17:08:29 +0800

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import NavBar from "../components/Navbar/Navbar";
+import Main from "../components/Main/Main";
+import Body from "../components/body/body";
+import Card from "../components/Card/Card";
+import Sig from "../components/Signature/Sig";
+
+export default function Home() {
+
+  return React.createElement(
+    "div",
+    {id:'top'},
+    <NavBar />,
+    <Main />,
+    <Body />,
+    <Sig />
+  );
+// }
+
+};
+
