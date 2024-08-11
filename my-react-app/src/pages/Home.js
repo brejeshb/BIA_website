ffb57288ee7eda6312050df55166aa48f3499ac7
brejeshb <155944318+brejeshb@users.noreply.github.com>
2024-08-11 19:28:35 +0800

    Add files via upload

---
+++ my-react-app/src/pages/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import NavBar from "../components/Navbar/Navbar";
+import Main from "../components/Main/Main";
+import Body from "../components/body/body";
+import Card from "../components/Card/Card";
+import Sig from "../components/Signature/Sig";
+import Countdown from "../components/Countdown/Countdown";
+
+
+
+export default function Home() {
+
+  return React.createElement(
+    "div",
+    {id:'top'},
+    <NavBar />,
+    <Main />,
+    <Countdown/>,
+
+    <Body />,
+    <Sig />
+    
+  );
+// }
+
+};
+
